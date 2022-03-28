ae606f03a5f680ed121da7b5640f11f68f055eb1
KingAlex1997 <alex97l@hotmail.com>
2022-03-28 18:32:10 -0300

    controlador de transaccion

--- app/controllers/transaccions_controller.rb
+++ app/controllers/transaccions_controller.rb
@@ -1,2 +1,44 @@
 class TransaccionsController < ApplicationController
+ before_action :set_transaccion, only:[:show]
+
+  def index
+    @user = current_user
+      @transaccions = policy_scope(transaccion).order(created_at: :desc)
+  end
+
+  def show
+    authorize @transaccion
+  end
+
+  def new
+    @entrada = Entrada.find(params[:id])
+    @transaccion = transaccion.new
+    authorize @transaccion
+    authorize @entrada
+  end
+
+  def create
+    @transaccion = transaccion.new(transaccion_params)
+    authorize @transaccion
+    @transaccion.entrada = Event.find(params[:entrada_id])
+    @transaccion.user = current_user
+    if @transaccion.save
+      redirect_to transaccion_path(@transaccion)
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def set_transaccion
+    @transaccion = transaccion.find(params[:id])
+  end
+
+  def transaccion_params
+    params.require(:transaccion).permit(:pagada)
+  end
+  
+  # I m just checking if the push origin master is still working after some config
+end
 end
