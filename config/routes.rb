100cba3e267603e7428f03545cb9e4d2c5fbc2c5
KingAlex1997 <alex97l@hotmail.com>
2022-03-28 17:40:27 -0300

    se agregan las rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :events do
+    resources :entrada, only: [:new, :create] do
+      resources :transaccion, only: [:new, :create]
+    end
+  end
+  resources :entrada, only: [:show, :index, :edit, :update, :destroy ]
+  resources :transaccion, only: [:show, :index]
+  resources :user, only: [:index, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
